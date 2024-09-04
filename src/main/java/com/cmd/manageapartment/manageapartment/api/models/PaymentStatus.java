9ead76302db83e4492a1a92e57c8740dcc310c73
CaoMinhDuc21002200 <caominhduc_t66@hus.edu.vn>
2024-09-04 09:38:46 +0700

    Delete src directory

--- src/main/java/com/cmd/manageapartment/manageapartment/api/models/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.cmd.manageapartment.manageapartment.api.models;
-
-public enum PaymentStatus {
-    PENDING,
-    NOT_PAID,
-    COMPLETED
-}
-
--- src/main/java/com/cmd/manageapartment/manageapartment/api/service/ResidentNotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.cmd.manageapartment.manageapartment.api.service;
-
-import org.springframework.stereotype.Service;
-
-public interface ResidentNotificationService {
-
-    public void notifyResidentAboutFees();
-}
